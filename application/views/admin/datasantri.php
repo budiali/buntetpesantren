0647f926007e4501e353b8ed46115a678d0b1232
budiali96 <vj.boedie@gmail.com>
2018-12-27 02:05:08 +0700

    merubah data table

--- application/views/admin/datasantri.php
+++ application/views/admin/datasantri.php
@@ -6,18 +6,17 @@
 
 <?php $this->load->view('_includes/sidebar'); ?>
 
- <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
+<div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Data Santri</h1>
+            <h1>Data Tables</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="<?php echo base_url().'dashboard'; ?>">Home</a></li>
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Data Tables</li>
             </ol>
           </div>
@@ -25,42 +24,45 @@
       </div><!-- /.container-fluid -->
     </section>
 
-  <section class="content">
-   <div class="row">
-    <div class="col-12">
-     <div class="card">
+     <section class="content">
+      <div class="row">
+        <div class="col-12">
+          <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Data Santri</h3>
+              <h3 class="card-title">Data Table With Full Features</h3>
             </div>
-            <!-- /.card-header -->
-            <div class="card-body">
+
+             <div class="card-body">
               <table id="example1" class="table table-bordered table-striped">
-                
-                <tr>
+                <thead>
+                  <tr>
                   <th>No</th>
                   <th>NIS</th>
                   <th>NAMA SANTRI</th>
                   <th>TEMPAT LAHIR</th>
                   <th>TANGGAL LAHIR</th>
                   <th>ALAMAT</th>
+                  <th>ACTION</th>
                 </tr>
-
-                <?php 
+                </thead>
+                  <?php 
                 $id = 1;
                 foreach($santri as $row) :
                  ?>
-                
-                <tr>
-                	<td><?php echo $id++; ?></td>
-                	<td><?php echo $row -> nis; ?></td>
-                	<td><?php echo $row -> nama_santri; ?></td>
-                	<td><?php echo $row -> tempat_lahir_santri; ?></td>
-                	<td><?php echo $row -> tanggal_lahir_santri; ?></td>
-                	<td><?php echo $row -> alamat_santri; ?></td>
+                <tbody>
+                   <tr>
+                  <td><?php echo $id++; ?></td>
+                  <td><?php echo $row -> nis; ?></td>
+                  <td><?php echo $row -> nama_santri; ?></td>
+                  <td><?php echo $row -> tempat_lahir_santri; ?></td>
+                  <td><?php echo $row -> tanggal_lahir_santri; ?></td>
+                  <td><?php echo $row -> alamat_santri; ?></td>
+                  <td>
+                    <a href="#" class="badge badge-info"><span class="glyphicon glyphicon-trash"></span>Lihat Detail</a>
+                  </td>
                 </tr>
-
-       			<?php endforeach; ?>
-                
+                </tbody>
+                <?php endforeach; ?>
               </table>
             </div>
             <!-- /.card-body -->
@@ -72,9 +74,40 @@
       <!-- /.row -->
     </section>
     <!-- /.content -->
-
+  </div>
+  <!-- /.content-wrapper -->
 
 
 
 <?php $this->load->view('_includes/footer'); ?>
 
+  <script src="<?php echo base_url('assets/LTE/')?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url('assets/LTE/')?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="<?php echo base_url('assets/LTE/')?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url('assets/LTE/')?>plugins/datatables/dataTables.bootstrap4.min.js"></script>
+<!-- SlimScroll -->
+<script src="<?php echo base_url('assets/LTE/')?>plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo base_url('assets/LTE/')?>plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url('assets/LTE/')?>dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?php echo base_url('assets/LTE/')?>dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false
+    });
+  });
+</script>
+
+ 
