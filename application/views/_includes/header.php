45c9313846e2227ff0e00819719c9c41ca1e3c96
budiali96 <vj.boedie@gmail.com>
2018-12-28 22:47:14 +0700

    modified

--- application/views/_includes/header.php
+++ application/views/_includes/header.php
@@ -9,9 +9,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/LTE/plugins/font-awesome/css/font-awesome.min.css'?>">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
+   <link rel="stylesheet" href="<?php echo base_url('assets/LTE/')?>plugins/datatables/dataTables.bootstrap4.min.css">
+   <link rel="stylesheet" href="<?php echo base_url('assets/LTE')?>plugins/iCheck/all.css">
   <link rel="stylesheet" href="<?php echo base_url().'assets/LTE/dist/css/adminlte.min.css'?>">
   <!-- iCheck -->
   <link rel="stylesheet" href="<?php echo base_url().'assets/LTE/plugins/iCheck/flat/blue.css'?>">
